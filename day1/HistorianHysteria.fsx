ce1f02025a3cc65c7c69d3dc41acdfe28b146671
biergit <l4143-github@yahoo.de>
2024-12-03 08:32:50 +0000

    Day 1

---
+++ day1/HistorianHysteria.fsx
@@ -0,0 +1,23 @@
+﻿open System.IO
+
+let lines = File.ReadAllLines "input.txt"
+let sortedNumbers = lines |> Array.map (fun line -> line.Split("   "))
+                    |> Array.map (fun parts -> Array.map int parts)
+                    |> Array.map (fun parts -> (parts.[0], parts.[1]))
+                    |> Array.unzip
+                    |> fun (a, b) -> (Array.sort a, Array.sort b)
+                    |> fun (a, b) -> Array.zip a b
+printfn "Total distance: %d" (sortedNumbers |> Array.map (fun (a, b) -> abs (a - b)) |> Array.sum)
+ 
+let similarityScore = 
+    sortedNumbers |> Array.unzip 
+            |> fun (a, b) 
+                -> (a, (b |> Array.countBy id) |> Map.ofArray)
+                |> fun (a, b) 
+                    -> a |> Array.map (fun number -> (number, Map.tryFind number b |> Option.defaultValue 0))
+                         |> Array.map (fun (a, b) -> a*b) |> Array.sum
+
+printfn "Similarity: %d" similarityScore
+ 
+ 
+
